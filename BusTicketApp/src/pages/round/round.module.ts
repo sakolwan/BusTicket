2a76a99b006754aaaa3b9a9554c2cefbdef6a113
sakolwan <sakolvan_design@hotmail.com>
2019-03-19 16:50:17 +0700

    create app

---
+++ BusTicketApp/src/pages/round/round.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RoundPage } from './round';
+
+@NgModule({
+  declarations: [
+    RoundPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RoundPage),
+  ],
+})
+export class RoundPageModule {}
